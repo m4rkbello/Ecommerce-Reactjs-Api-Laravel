0132975ce30ea5bf3c64957e030bed0322ad42f7
M4rk <89385349+m4rkbello@users.noreply.github.com>
2023-10-09 01:13:19 +0800

    Your commit message

--- laravel-backend/app/Http/Controllers/UserController.php
+++ laravel-backend/app/Http/Controllers/UserController.php
@@ -12,14 +12,22 @@ class UserController extends Controller
     function register(Request $req)
     {
         // return "Mekus-Mekus mo nayan heh?!";
-     
-
+    
         $user = new User;
         $user->name= $req->input('name');
         $user->email= $req->input('email');
         $user->password= Hash::make ($req->input('password'));
         $user->save();
+        return $user;
+    }
 
+    function login(Request $req)
+    {
+        $user= User::where('email',$req->email)->first();
+        if(!$user || !Hash::check($req->password,$user->password))
+        {
+            return ["error"=>"Email or password is not match!"];
+        }
         return $user;
     }
 }
